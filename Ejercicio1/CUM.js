04f6d9920666924c0d7a172be6fca91f57865d9a
Carislost <villaloboscarlos213@gmail.com>
2023-08-28 20:19:32 -0600

    Ejercicio1
    Cambios ligeros y correccion de error en el color

--- Ejercicio1/CUM.js
+++ Ejercicio1/CUM.js
@@ -1,4 +1,3 @@
-// Inicializa la tabla y variables
 const tableBody = document.getElementById('Tabla').getElementsByTagName('tbody')[0];
 let Notas = [];
 
@@ -11,14 +10,13 @@ while (true) {
     // Almacena los datos
     Notas.push({ materia, notaFinal, UV });
 
-    // Añade una fila a la tabla
-    let row = tableBody.insertRow();
-    let fila1 = row.insertCell(0);
-    let fila2 = row.insertCell(1);
-    let fila3 = row.insertCell(2);
-    fila1.innerHTML = materia;
-    fila2.innerHTML = notaFinal;
-    fila3.innerHTML = UV;
+    let fila = tableBody.insertRow();
+    let celda1 = fila.insertCell(0);
+    let celda2 = fila.insertCell(1);
+    let celda3 = fila.insertCell(2);
+    celda1.innerHTML = materia;
+    celda2.innerHTML = notaFinal;
+    celda3.innerHTML = UV;
 
     // Pregunta si se debe continuar
     if (!confirm('Deseas agregar otra materia')) {
@@ -26,25 +24,24 @@ while (true) {
     }
 }
 
+// Calcula el CUM
 if(calcularCum(Notas) >= 6.0){
     document.getElementById("nota").style.color = "green";
 }else{
     document.getElementById("nota").style.color = "red";
 }
 
-// Calcula el CUM
 function calcularCum(Notas) {
-    let totalPoints = 0;
-    let totalUVs = 0;
-    for (let notaFinal of Notas) {
-        totalPoints += notaFinal.notaFinal * notaFinal.UV;
-        totalUVs += notaFinal.UV;
+    let unidadesMerito = 0;
+    let unidadesValorativas = 0;
+    for (let notaFin of Notas) {
+        unidadesMerito += notaFin.notaFinal * notaFin.UV;
+        unidadesValorativas += notaFin.UV;
     }
 
-    // Muestra el resultado
-    return totalPoints / totalUVs;
+    // Devuelve el resultado
+    return unidadesMerito / unidadesValorativas;
 }
 
 cum = calcularCum(Notas);
 document.getElementById('nota').innerHTML = '<h2>CUM:' + cum.toFixed(2) + '</h2>';
-
